1b796c9de650cd7c36dab89dfdd594902c2b3ab5
Luis Saavedra <lenrique.saavedra@gmail.com>
2019-10-24 16:34:38 -0600

    finished

---
+++ script.js
@@ -0,0 +1,65 @@
+const gridContainer = document.querySelector('.gridContainer');
+const black = document.querySelector('#black');
+const rainbow = document.querySelector('#rainbow');
+const reset = document.querySelector('#reset');
+
+let gridSize = 16;
+
+function createGrid() {
+    let gridRows = gridSize;
+    let gridCols = gridSize;
+
+    for (let i = 0; i < gridRows; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+        gridContainer.appendChild(row);
+        for (let j = 0; j < gridCols; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            row.appendChild(cell);
+        };
+    }; 
+};
+createGrid();
+
+black.addEventListener('click', () => {
+    let cells = Array.from(document.querySelectorAll('.grid-cell'));
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
+    });
+});
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    };
+    return color;
+};
+
+rainbow.addEventListener('click', () => {
+    let cells = Array.from(document.querySelectorAll('.grid-cell'));
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover',() => {
+            cell.style.backgroundColor = getRandomColor();
+        });
+    });
+});
+
+reset.addEventListener('click', () => {
+    gridSize = parseInt(prompt('Grid size?:'));
+    if (isNaN(gridSize)) {   
+        let cells = Array.from(document.querySelectorAll('.grid-cell'));
+        cells.forEarch((cell) => {
+            cell.style.backgroundColor = '';
+        });
+    } else {
+        while (gridContainer.hasChildNodes()) {
+            gridContainer.removeChild(gridContainer.lastChild);
+        }
+        createGrid();
+    }
+});
